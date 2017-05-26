5ad975fad5e186eac4d1dbefeb7a2e63d115dd31
de <git.daniele@gmail.com>
2017-05-26 16:05:30 +0200

    type not required

--- src/hashfuck.rs
+++ src/hashfuck.rs
@@ -50,7 +50,7 @@ fn hash_until_ff<HashAlg: Digest>(origin_hash: &str, mut hasher: HashAlg) -> Str
 }
 
 pub fn compile_hashfuck(program: String) -> String {
-    let splitted: Vec<&str> = program.split(":").collect::<Vec<_>>();
+    let splitted: Vec<&str> = program.split(":").collect();
     match splitted.len() {
         2 => {
             let algorithm = splitted[0];
